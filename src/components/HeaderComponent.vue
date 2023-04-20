60e50bc75f853a015a6b89bfd5fe25475304deca
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-20 16:43:33 +0200

    modify HeaderComponent.vue

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,18 +1,39 @@
 <template>
-    <div>
-        <h1>BoolFlix</h1>
-        <SearchComponent />
-    </div>
+    <header>
+        <div class="container d-flex justify-content-between align-items-center">
+            <div id="logo_container">
+                <a href="#"><img class="w-100" src="../img/boolflix.png" alt="boolflix_logo"></a>
+            </div>
+            <div id="input_container">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-danger" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
-import SearchComponent from './SearchComponent.vue';
+import { store } from '../data/store';
 export default {
     name: 'HeaderComponent',
-    components: {
-        SearchComponent
+    data() {
+        return {
+            store
+        }
     }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    height: 7.5rem;
+
+    #logo_container {
+        width: 13rem;
+        height: 8rem;
+        padding-top: 2.2rem;
+    }
+}
+</style>
