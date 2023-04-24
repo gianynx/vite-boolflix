df6916eb222e94b25774176c4c463063b59cfe0c
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-24 16:07:22 +0200

    add flip card effect

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="card p-3">
-            <ul class="list-unstyled text-white">
+        <div class="flip_card">
+            <div class="flip_card_inner">
                 <div id="img_container">
                     <img class="img-fluid" :src="store.imgPath + store.imgSize + image" alt="">
                 </div>
-                <div id="info_container" class="pt-3 text-center">
-                    <li class="fs-5">{{ title }}</li>
-                    <li class="pt-1">
+                <div id="info_container" class="pt-3 text-center text-white">
+                    <div class="fs-5">{{ title }}</div>
+                    <div class="pt-1">
                         <img class="flag_img" :src="'/flag_icons/' + original_language + '.png'" :alt="original_language">
-                    </li>
-                    <li class="pt-1">Vote average: {{ vote_average }}</li>
+                    </div>
+                    <div class="pt-1">Vote average: {{ vote_average }}</div>
                 </div>
-            </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -36,17 +36,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card {
-    height: 36rem;
+.flip_card {
+    height: 30rem;
+    cursor: pointer;
+    background-color: transparent;
+    perspective: 1000px;
 
-    img {
-        height: 351px;
+    &:hover {
+        #img_container {
+            display: none;
+        }
+
+        #info_container {
+            display: block;
+        }
     }
+}
+
+.flip_card_inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+}
+
+.flip_card:hover .flip_card_inner {
+    transform: rotateY(180deg);
+}
+
+#img_container,
+#info_container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+}
+
+#info_container {
+    transform: rotateY(180deg);
 
     .flag_img {
         width: 20px;
         height: 20px;
     }
+}
+
+img {
+    height: 30rem;
 }
 
 @media screen and (max-width: 575px) {
